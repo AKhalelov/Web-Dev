9c65318d40bfbf0a2c60244b4a2268f95d0632ea
Aisultan Khalelov <a_khalelov@kbtu.kz>
2023-02-25 15:25:36 +0500

    Added categories

---
+++ Lab5/src/app/categories.ts
@@ -0,0 +1,10 @@
+export interface Category {
+    name: string;
+}
+
+export const categories: Category[] = [
+    { name: "IPhones"}, 
+    { name: "MacBooks"}, 
+    { name: "Apple Watches"}, 
+    { name: "AirPods"}
+]
--- Lab5/src/app/products.ts
+++ Lab5/src/app/products.ts
@@ -1,3 +1,5 @@
+import { Category } from "./categories";
+
 export interface Product {
   id: number;
   name: string;
@@ -107,4 +109,5 @@ export const products = [
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https:/
+can be found in the LICENSE file at https://angular.io/license
+*/
