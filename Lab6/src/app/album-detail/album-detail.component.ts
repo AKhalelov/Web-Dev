9b7a97b1e89600b2ca91a9f11e65fcba9d5b06f8
Aisultan Khalelov <a_khalelov@kbtu.kz>
2023-03-08 00:37:46 +0500

    Added album service. getAlbums(), getAlbum() functions

--- Lab6/src/app/album-detail/album-detail.component.ts
+++ Lab6/src/app/album-detail/album-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { ALBUMS } from '../fake-db';
+import { Album } from '../models';
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
   styleUrls: ['./album-detail.component.css']
 })
-export class AlbumDetailComponent {
+
+export class AlbumDetailComponent implements OnInit{
+  album: Album;
+
+  constructor(private route: ActivatedRoute,
+    private albumService: AlbumService) { // ActivatedRoute is a injectable class, that's why we don't need to create instance with 'new'
+    this.album = {} as Album;
+  }
+  
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      // this.album = ALBUMS.find((album: Album) => album.id === id) as Album;
+      this.albumService.getAlbum(id).subscribe((album) => {
+        this.album = album;
+      })
+    });
+  }
+  // const id = Number(this.route.snapshot.paramMap.get('id));
 
 }
