e654a85fcc9457fa70966f44fb801564f57eeddf
Aisultan Khalelov <a_khalelov@kbtu.kz>
2023-03-08 00:45:14 +0500

    Added boolean variable loaded

--- Lab6/src/app/album-detail/album-detail.component.ts
+++ Lab6/src/app/album-detail/album-detail.component.ts
@@ -12,20 +12,24 @@ import { Album } from '../models';
 
 export class AlbumDetailComponent implements OnInit{
   album: Album;
+  loaded: boolean;
 
   constructor(private route: ActivatedRoute,
     private albumService: AlbumService) { // ActivatedRoute is a injectable class, that's why we don't need to create instance with 'new'
     this.album = {} as Album;
+    this.loaded = true;
   }
   
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.route.paramMap.subscribe((params) => {
       const id = Number(params.get('id'));
-      // this.album = ALBUMS.find((album: Album) => album.id === id) as Album;
+      this.loaded = false;
       this.albumService.getAlbum(id).subscribe((album) => {
         this.album = album;
+        this.loaded = true;
       })
+      // this.album = ALBUMS.find((album: Album) => album.id === id) as Album;
     });
   }
   // const id = Number(this.route.snapshot.paramMap.get('id));
