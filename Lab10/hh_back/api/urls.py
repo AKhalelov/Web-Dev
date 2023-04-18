170ffe15238a04132fc2c31e58864ef9b52be197
aissultan <a_khalelov@kbtu.kz>
2023-04-18 22:07:53 +0600

    Fixed views

--- Lab10/hh_back/api/urls.py
+++ Lab10/hh_back/api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 from api import views
 
 urlpatterns = [
     path('companies/', views.get_companies),
     path('companies/<int:id>', views.get_company),
     path('companies/<int:id>/vacancies', views.get_company_vacancies),
-    path('vacancies', views.get_vacancies),
-    path('vacancies/<int:id>', views.get_vacancy),
+    path('vacancies', views.VacanciesAPIView.as_view()),
+    path('vacancies/<int:id>', views.VacancyDetailAPIView.as_view()),
     path('vacancies/top_ten', views.top_ten_vacancies)
 ]
--- Lab10/hh_back/api/views.py
+++ Lab10/hh_back/api/views.py
@@ -1,45 +1,95 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api.serializers import CompanySerializer, VacancySerializer
 from api.models import Company, Vacancy
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def get_companies(request):
     if request.method == 'GET':
         companies = Company.objects.all()
-        
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
 
+    if request.method == 'POST':
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    companies = Company.objects.all()
-    companies_json = [comp.to_json() for comp in companies]
-    return JsonResponse(companies_json, safe=False, json_dumps_params={'indent': 2})
-
-def get_company(request, id):
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_company(request, id): 
     try:
         company = Company.objects.get(id=id)
-        return JsonResponse(company.to_json(), status=200, json_dumps_params={'indent': 2})
     except Company.DoesNotExist as error:
-        return JsonResponse({'error': str(error)}, status=400, json_dumps_params={'indent': 2})
+        return Response({'error': str(error)}, status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = CompanySerializer(instance=company, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        company.delete()
+        return Response({'deleted': True}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
 def get_company_vacancies(request, id):
-    try:
-        company = Company.objects.get(id=id)
-        vacancies = Vacancy.objects.filter(company=company)
-        vacancies_json = [vacancy.to_json() for vacancy in vacancies]
-        return JsonResponse(vacancies_json, safe=False, json_dumps_params={'indent': 2})
-    except Company.DoesNotExist as error:
-        return JsonResponse({'error': str(error)}, status = 400, json_dumps_params={'indent': 2})
+    if request.method == 'GET':
+        needed_company = Company.objects.get(id=id)
+        vacancies = Vacancy.objects.filter(company=needed_company)
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-def get_vacancies(request):
-    vacancies = Vacancy.objects.all()
-    vacancies_json = [vacancy.to_json() for vacancy in vacancies]
-    return JsonResponse(vacancies_json, safe=False, json_dumps_params={'indent': 2})
+class VacanciesAPIView(APIView):
+    def get(self, request):
+        vacancies = Vacancy.objects.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-def get_vacancy(request, id):
-    try:
-        vacancy = Vacancy.objects.get(id=id)
-        return JsonResponse(vacancy.to_json(), status=200, json_dumps_params={'indent': 2})
-    except Vacancy.DoesNotExist as error:
-        return JsonResponse({'error': str(error)}, status=400, json_dumps_params={'indent': 2})
+    def post(self, request):
+        serializer = VacancySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class VacancyDetailAPIView(APIView):
+    def get_object(self, id):
+        try:
+            return Vacancy.objects.get(id=id)
+        except Vacancy.DoesNotExist as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id):
+        vacancy = self.get_object(id)
+        serializer = VacancySerializer(vacancy)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        vacancy = self.get_object(id)
+        serializer = VacancySerializer(instance=vacancy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        vacancy = self.get_object(id)
+        vacancy.delete()
+        return Response({'deleted': True}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
 def top_ten_vacancies(request):
-    vacancies = Vacancy.objects.all().order_by('-salary')[:10]
-    return JsonResponse([vacancy.to_json() for vacancy in vacancies], safe=False, json_dumps_params={'indent': 2})
+    if request.method == 'GET':
+        vacancies = Vacancy.objects.all().order_by('-salary')[:10]
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
